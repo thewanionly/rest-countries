7155ac7820eeb7068a74ee57b8401f98136addba
thewanionly <wani.pitogo@gmail.com>
2021-10-06 06:04:03 +0800

    Rough implementation: Fetch from countries API. Display all countries.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,79 @@
+import { useState, useEffect } from 'react'
+
+// config
+const API_ENDPOINT = 'https://restcountries.com/v2'
+
+// fetch from API
+const fetchFromUrl = async url => {
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    if (!response.ok) throw new Error(`Error fetching (${response.status})`)
+
+    return data
+  } catch (error) {
+    console.error('Error logged here: ', error)
+    throw error
+  }
+}
+
 const App = () => {
-  return <div>Hello App</div>
+  const [countries, setCountries] = useState()
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    // fetch countries
+    const fetchCountries = async () => {
+      try {
+        setLoading(true)
+        const data = await fetchFromUrl(`${API_ENDPOINT}/all`)
+
+        setCountries(data)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+        setLoading(false)
+      }
+    }
+
+    fetchCountries()
+  }, [])
+
+  console.log('App countries', countries)
+  console.log('App countries is array', Array.isArray(countries))
+
+  console.log('App error', error)
+
+  console.log('App loading', loading)
+
+  return (
+    <div>
+      {countries ? (
+        <>
+          <h2>List of Countries</h2>
+          <ul>
+            {countries.map(country => (
+              <CountriesCard key={country.alpha2Code} data={country} />
+            ))}
+          </ul>
+        </>
+      ) : error ? (
+        <h1>{`There's an error: ${error}`}</h1>
+      ) : (
+        loading && <h1>Loading...</h1>
+      )}
+    </div>
+  )
+}
+
+const CountriesCard = ({ data }) => {
+  return (
+    <li>
+      <span>{data.name}</span>
+    </li>
+  )
 }
 
 export default App
