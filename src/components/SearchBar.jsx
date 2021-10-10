81b0024fb5b1a39ec362158f395bc412a5ec40bb
thewanionly <wani.pitogo@gmail.com>
2021-10-11 01:24:32 +0800

    Add clear input button in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,19 @@
 import Icon from 'components/Icon.jsx'
 
-const SearchBar = ({ value, placeholder = 'Search...', onChange }) => {
+const SearchBar = ({ value, placeholder = 'Search...', onChange: handleSetSearchTerm }) => {
+  const handleSearch = e => {
+    handleSetSearchTerm(e.target.value)
+  }
+
+  const handleClearInput = () => {
+    handleSetSearchTerm('')
+  }
+
   return (
     <div className='search-bar'>
       <Icon name='search' />
-      <input type='text' placeholder={placeholder} value={value} onChange={onChange} />
+      <input type='text' placeholder={placeholder} value={value} onChange={handleSearch} />
+      {value && <Icon name='close' onClick={handleClearInput} />}
     </div>
   )
 }
