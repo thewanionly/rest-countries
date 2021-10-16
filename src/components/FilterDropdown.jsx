d4ef5afc26268ebd85e0c3ba6cc6579a58de77f0
thewanionly <wani.pitogo@gmail.com>
2021-10-17 04:58:25 +0800

    Custom drodpown (don't use native select, it's hard to style)

--- src/components/FilterDropdown.jsx
+++ src/components/FilterDropdown.jsx
@@ -1,25 +1,45 @@
+import { useState } from 'react'
+
+import Icon from 'components/Icon'
+
 const FilterDropdown = ({
   options,
   value,
   placeholder = 'Filter',
   onChange: handleSetFilterValue
 }) => {
-  const handleFilter = e => {
-    handleSetFilterValue(e.target.value)
+  const [isOptionsOpen, setIsOptionsOpen] = useState(false)
+
+  const handleToggleOptions = () => {
+    setIsOptionsOpen(prevValue => !prevValue)
+  }
+
+  const handleSelectOption = value => {
+    handleSetFilterValue(value)
+    setIsOptionsOpen(false)
   }
 
   return (
     <div className='filter-dropdown'>
-      <select value={value} onChange={handleFilter}>
-        <option value='' disabled selected>
-          {placeholder}
-        </option>
+      <div className='filter-dropdown__select' onClick={handleToggleOptions}>
+        <div className='filter-dropdown__select__text'>{value || placeholder}</div>
+        <Icon
+          className='filter-dropdown__select__arrow-icon'
+          name={isOptionsOpen ? 'expand_less' : 'expand_more'}
+        />
+      </div>
+      <div className={`filter-dropdown__options${!isOptionsOpen ? ' hide' : ''}`}>
         {options?.map(option => (
-          <option key={option} value={option}>
-            {option}
-          </option>
+          <div
+            className={`filter-dropdown__options__item${option === 'Show all' ? ' break' : ''}`}
+            key={option}
+            value={option}
+            onClick={() => handleSelectOption(option)}
+          >
+            {option === 'Show all' ? <em>{option}</em> : option}
+          </div>
         ))}
-      </select>
+      </div>
     </div>
   )
 }
