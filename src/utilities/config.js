893b1ef2b1445f1faf7906c86f4a9bc6042bce6f
thewanionly <wani.pitogo@gmail.com>
2021-10-18 00:54:40 +0800

    Update countries card size and image

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -1,6 +1,6 @@
 const API_ENDPOINT = 'https://restcountries.com/v2'
 const USER_LOCALE = navigator.language
 const TIMEOUT_SEC = 10
-const PAGE_LIMIT = 10
+const PAGE_LIMIT = 8
 
 export { API_ENDPOINT, USER_LOCALE, TIMEOUT_SEC, PAGE_LIMIT }
