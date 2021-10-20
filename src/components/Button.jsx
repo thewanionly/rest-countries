d20f203ddc907ccef071d6fca34edd78320e565c
thewanionly <wani.pitogo@gmail.com>
2021-10-21 04:35:41 +0800

    Add propTypes to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,8 @@
+import PropTypes from 'prop-types'
+
 import Icon from 'components/Icon'
 
-const Button = ({ className = '', label, icon, iconPos = 'right', onClick, isTransparent }) => {
+const Button = ({ className = '', label, icon, isTransparent, onClick }) => {
   const iconProps = typeof icon === 'object' ? { ...icon } : { name: icon }
 
   return (
@@ -12,6 +14,14 @@ const Button = ({ className = '', label, icon, iconPos = 'right', onClick, isTra
       {label}
     </button>
   )
+}
+
+Button.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.string,
+  icon: PropTypes.oneOfType([PropTypes.string, PropTypes.shape(Icon.propTypes)]),
+  isTransparent: PropTypes.bool,
+  onClick: PropTypes.func
 }
 
 export default Button
