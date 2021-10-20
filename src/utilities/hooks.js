ee5801ae6fc1bae8d59fbda9a2ef72634a7f8be7
thewanionly <wani.pitogo@gmail.com>
2021-10-21 05:11:51 +0800

    Create useClickOutside hook. Add comments to other hooks as well

--- src/utilities/hooks.js
+++ src/utilities/hooks.js
@@ -1,9 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import { fetchFromUrl } from 'utilities/helpers'
 
 const toAlwaysFetch = false //for dev/testing purposes only, set to default in production
 
+/**
+ * Fetches data from specified url, stores in localStorage, sets a state and return that state.
+ * If data exists in localStorage, don't fetch.
+ *
+ * @param {string} url - The API endpoint url to fetch data from
+ * @returns {Array} - [
+ *   data - results of data fetching
+ *   isLoading - loading state of the fetching
+ *   error - error received from fetching
+ * ]
+ */
 const useLoadData = url => {
   const [data, setData] = useState()
   const [error, setError] = useState(false)
@@ -51,6 +62,16 @@ const useLoadData = url => {
   return [data, isLoading, error]
 }
 
+/**
+ * Sets dark mode state depending on either localStorage, browser theme or explicit
+ * clicking of the user of the dark mode button.
+ *
+ * @param none
+ * @returns {Array} - [
+ *   darkMode - dark mode state
+ *   handleToggleDarkMode - function that updates darkmode
+ * ]
+ */
 const useDarkMode = () => {
   // 1. Get data from localStorage
   let isDarkModeInLocalStorage = localStorage.getItem('darkMode')
@@ -76,4 +97,31 @@ const useDarkMode = () => {
   return [darkMode, handleToggleDarkMode]
 }
 
-export { useLoadData, useDarkMode }
+/**
+ * Detects if the user clicks outside the specified ref/element.
+ *
+ * @param {function} action - Callback function to execute when user clicks outside
+ * @returns {ref} - Reference to the element
+ */
+const useClickOutside = action => {
+  const elementRef = useRef()
+
+  useEffect(() => {
+    const handleClick = e => {
+      if (elementRef?.current && !elementRef.current.contains(e.target)) {
+        // call action callback when user clicked outside
+        action?.()
+      }
+    }
+
+    window.addEventListener('click', handleClick)
+
+    return () => {
+      window.removeEventListener('click', handleClick)
+    }
+  }, [elementRef, action])
+
+  return elementRef
+}
+
+export { useLoadData, useDarkMode, useClickOutside }
