58dd16d9e96f22ae94fe695012e9923b1b4350a8
thewanionly <wani.pitogo@gmail.com>
2021-10-22 05:39:09 +0800

    No need for useCountriesData

--- src/utilities/hooks/index.js
+++ src/utilities/hooks/index.js
@@ -1,7 +1,6 @@
 import useClickOutside from './useClickOutside'
-import useCountriesData from './useCountriesData'
 import useDarkMode from './useDarkMode'
 import useData from './useData'
 import useLoadData from './useLoadData'
 
-export { useClickOutside, useCountriesData, useDarkMode, useData, useLoadData }
+export { useClickOutside, useDarkMode, useData, useLoadData }
